3c59fd0db8ea1b8f5a41cb902dca6e0f2d357629
Ankush Mishra <ankushmishra.candidate@gmail.com>
2025-03-04 22:01:11 +0530

    29.cpp

---
+++ 29.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(string str) {
+    int left = 0, right = str.length() - 1;
+    while (left < right) {
+        if (str[left] != str[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+    if (isPalindrome(str)) cout << "Palindrome";
+    else cout << "Not a palindrome";
+    return 0;
+}
