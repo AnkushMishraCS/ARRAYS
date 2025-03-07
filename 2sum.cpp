d532508e12eff76a3d247f4e63aca93b2db0fe45
Ankush Mishra <ankushmishra.candidate@gmail.com>
2025-03-07 20:33:23 +0530

    2sum.cpp

---
+++ 2sum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> m;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (m.find(complement) != m.end()) 
+            return {m[complement], i};
+        m[nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    if (!result.empty())
+        cout << result[0] << " " << result[1] << endl;
+    else
+        cout << "No solution found" << endl;
+    return 0;
+}
