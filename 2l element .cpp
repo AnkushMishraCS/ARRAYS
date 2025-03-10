b8f770cb8cd7d7cab5568a3e7c96e177fac8ead3
Ankush Mishra <ankushmishra.candidate@gmail.com>
2025-03-10 21:40:13 +0530

    2l element .cpp

---
+++ 2l element .cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int secondLargest(int arr[], int n) {
+    int first = INT_MIN, second = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+    return (second == INT_MIN) ? -1 : second;
+}
+
+int main() {
+    int arr[] = {12, 35, 1, 10, 34, 1};
+    cout << secondLargest(arr, 6);
+}
